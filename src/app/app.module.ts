9fcdd68672ef0ff6bbbf2c68f85cfb807a3be287
Mark Hagelberg <mark.hagelberg@gmail.com>
2018-02-17 16:49:29 -0700

    full form structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { AssetBuilderModule } from './asset-builder/asset-builder.module';
 import { CreatorFormComponent } from './asset-builder/creator-form/creator-form.component';
 import {MetaModule} from './meta/meta.module';
+import { HeaderComponent } from './header/header.component';
+import { AssetFormComponent } from './asset-form/asset-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreatorFormComponent
+    CreatorFormComponent,
+    HeaderComponent,
+    AssetFormComponent
   ],
   imports: [
     BrowserModule,
