c565199c24c7ed05b9e0cd1e0acc3562df3af6d5
Mark Hagelberg <mark.hagelberg@gmail.com>
2018-02-17 18:17:50 -0700

    don't need asset-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { AssetBuilderModule } from './asset-builder/asset-builder.module';
 import { CreatorFormComponent } from './asset-builder/creator-form/creator-form.component';
 import {MetaModule} from './meta/meta.module';
 import { HeaderComponent } from './header/header.component';
-import { AssetFormComponent } from './asset-form/asset-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreatorFormComponent,
-    HeaderComponent,
-    AssetFormComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AssetBuilderModule,
     MetaModule
   ],
   providers: [],
