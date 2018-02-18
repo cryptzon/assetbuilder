87e06bafc60cc1cfcba54b40508f0116673ae311
Mark Hagelberg <mark.hagelberg@gmail.com>
2018-02-17 20:44:02 -0700

    fix build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CreatorFormComponent } from './asset-builder/creator-form/creator-form.component';
-import {MetaModule} from './meta/meta.module';
 import { HeaderComponent } from './header/header.component';
+import { UtilModule } from './util/util.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MetaModule
+    UtilModule
   ],
   providers: [],
   bootstrap: [AppComponent]
